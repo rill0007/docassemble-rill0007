655045f471a19c0a4e56f43dc22306fef1022185
Zoe Rillstone <69031882+rill0007@users.noreply.github.com>
2021-05-04 17:18:35 +0930

    Welcome screen for app

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.rill0007',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.rill0007\n\nA docassemble extension.\n\n## Author\n\nZoe Rillstone, rill0007@flinders.edu.au\n\n',
+      long_description='# docassemble.rill0007\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nZoe Rillstone, rill0007@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Zoe Rillstone',
       author_email='rill0007@flinders.edu.au',
